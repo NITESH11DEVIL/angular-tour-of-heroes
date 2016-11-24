9115d8ae9764bfdfdabbc6beebdbbb5366fa33fa
Nitesh <nitesh.devil11@gmail.com>
2016-11-23 21:31:24 -0800

    third commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-export class Hero {
+import{Hero} from './hero';
+import {HeroDetailComponent} from './hero-detail.component';
+/*export class Hero {
   id :number;
   name : string;
-}
+}*/
 
 const HEROES: Hero[] = [
   {id: 10, name:'Devil'},
@@ -23,20 +25,13 @@ const HEROES: Hero[] = [
   template: `
   <h1>{{title}}</h1>
   <h2>My Heroes</h2>
-  <ul>
+  <ul class="heroes" >
       <li *ngFor="let hero of heroes"  [class.selected]="hero === selectedHero"
  (click)="onSelect(hero)">
       <span class="badge">{{hero.id}}</span>{{hero.name}}
       </li>
   </ul>
-<div *ngIf="selectedHero">
-  <h2>{{selectedHero.name}} details!</h2>
-  <div><label>id :</label>{{selectedHero.id}}</div>
-    <div>
-      <label>name :</label>
-      <input [(ngModel)]="selectedHero.name" placeholder="name">
-    </div>
-</div>
+<my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
  styles: [`
   .selected {
